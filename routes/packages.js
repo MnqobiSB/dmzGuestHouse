57d7b8dd9fb2a5733bbfccadc58f84476127bb0e
Amandla Mzolo <mnqobiwebdesign@gmail.comgit config --global user.email mnqobiwebdesign@gmail.comgit config --global user.email mnqobiwebdesign@gmail.com>
2023-09-26 13:23:32 +0200

    add package routes configurations in routes/packages.js

---
+++ routes/packages.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
+const { asyncErrorHandler, searchAndFilterPackages } = require('../middleware');
+const { isLoggedIn } = require('../middleware/users');
+const {
+	packageIndex,
+	packageNew,
+	packageCreate,
+	packageShow,
+	packageEdit,
+	packageUpdate,
+	packageDestroy
+} = require('../controllers/packages');
+
+/* GET packages Index /packages */
+router.get(
+	'/',
+	asyncErrorHandler(searchAndFilterPackages),
+	asyncErrorHandler(packageIndex)
+);
+
+/* GET packages New /packages/add-a-package */
+router.get('/add-a-package', isLoggedIn, packageNew);
+
+/* POST packages Create /packages */
+router.post(
+	'/',
+	isLoggedIn,
+	upload.array('images', 10),
+	asyncErrorHandler(packageCreate)
+);
+
+/* GET packages show /packages/:slug */
+router.get('/:slug', asyncErrorHandler(packageShow));
+
+/* GET packages edit /packages/:id/edit */
+router.get(
+	'/:id/edit',
+	isLoggedIn,
+	packageEdit
+);
+
+/* PUT packages update /packages/:id */
+router.put(
+	'/:id',
+	isLoggedIn,
+	upload.array('images', 10),
+	asyncErrorHandler(packageUpdate)
+);
+
+/* DELETE packages destroy /packages/:id */
+router.delete(
+	'/:id',
+	isLoggedIn,
+	asyncErrorHandler(packageDestroy)
+);
+
+module.exports = router;
