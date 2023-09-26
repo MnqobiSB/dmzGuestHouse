7e08c5318f10944ac197aa9303528551d8ef9d83
Amandla Mzolo <mnqobiwebdesign@gmail.comgit config --global user.email mnqobiwebdesign@gmail.comgit config --global user.email mnqobiwebdesign@gmail.com>
2023-09-26 11:40:22 +0200

    add index routes configurations in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,61 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { asyncErrorHandler } = require('../middleware');
+const {
+	landingPage,
+	subscribe,
+  postContact,
+  getBooking,
+  postBooking,
+	getAbout,
+	getCareers,
+	getSiteMap,
+	getTerms,
+	getDisclaimer,
+	getPrivacy
+} = require('../controllers');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* GET / - home page. */
+router.get(
+	'/',
+	asyncErrorHandler(searchAndFilterCars),
+	asyncErrorHandler(landingPage)
+);
+
+/* POST / - subscribe newsletter */
+router.post('/', asyncErrorHandler(subscribe));
+
+/* POST /contact */
+router.post('/', asyncErrorHandler(postContact));
+
+/* GET /booking */
+router.get('/booking', asyncErrorHandler(getBooking));
+
+/* POST /booking */
+router.post('/booking', asyncErrorHandler(postBooking));
+
+/* GET /about */
+router.get('/about-us', asyncErrorHandler(getAbout));
+
+/* GET /careers */
+router.get('/careers', asyncErrorHandler(getCareers));
+
+// Website Laws
+// ************
+
+/* GET /terms-of-service */
+router.get('/terms-of-service', asyncErrorHandler(getTerms));
+
+/* GET /legal-disclaimer */
+router.get('/legal-disclaimer', asyncErrorHandler(getDisclaimer));
+
+/* GET /privacy-policy */
+router.get('/privacy-policy', asyncErrorHandler(getPrivacy));
+
+// Website SEO
+// ************
+
+/* GET /site-map */
+router.get('/site-map', asyncErrorHandler(getSiteMap));
 
 module.exports = router;
