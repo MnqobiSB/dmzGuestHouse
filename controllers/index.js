ec37e1d51b0cd07ca25acd9ec2663acd7f7ce008
Amandla Mzolo <mnqobiwebdesign@gmail.comgit config --global user.email mnqobiwebdesign@gmail.comgit config --global user.email mnqobiwebdesign@gmail.com>
2023-12-18 23:13:10 +0200

    update booking email functionality

--- controllers/index.js
+++ controllers/index.js
@@ -63,7 +63,9 @@ module.exports = {
 
 				<p>Get 20% Discount for Online Bookings</p>
 
-				<p>Happy Vacations!,<br>DM-DMZ Guesthouse Newsletters</p>
+				<p>Happy Vacations!</p>
+
+				<p>DM-DMZ Guesthouse Newsletters</p>
 
 				<p><small><a href="https://www.dm-dmzguesthouse.co.za/contact-us">Unsubscribe</a> | <a href="https://www.dm-dmzguesthouse.co.za/privacy-policy">Privacy Policy</a> | <a href="https://www.dm-dmzguesthouse.co.za/terms-of-service">Terms Of Service</a> | <a href="https://www.dm-dmzguesthouse.co.za/contact-us">Contact Us</a></small></p>
 			`;
@@ -225,7 +227,9 @@ module.exports = {
 
 		  	<p>A reply will be sent to you as soon as your enquiry has been reviewed.</p>
 
-				<p>Happy Vacations,<br>DM-DMZ Guesthouse Contact Team</p>
+				<p>Happy Vacations!</p>
+
+				<p>DM-DMZ Guesthouse Contact Team</p>
 
 				<p><small><a href="https://www.dm-dmzguesthouse.co.za/privacy-policy">Privacy Policy</a> | <a href="https://www.dm-dmzguesthouse.co.za/terms-of-service">Terms Of Service</a> | <a href="https://www.dm-dmzguesthouse.co.za/contact-us">Contact Us</a></small></p>
 			`;
@@ -270,7 +274,7 @@ module.exports = {
 		const mailOptionsUser = {
 			from: '"DM-DMZ Guesthouse - Contact Us Team" <contact@dm-dmzguesthouse.co.za>',
 			to: req.body.email,
-			subject: 'Enquiry Recieved - DM-DMZ Gusethouse',
+			subject: 'Enquiry Recieved - DM-DMZ Guesthouse',
 			html: contactDataUser
 		};
 
@@ -303,31 +307,73 @@ module.exports = {
   // POST /booking
 	async postBooking (req, res, next) {
 		// send DM-DMZ Guesthouse an email
-		const contactData = `
-		  	<h1>New Customer Enquiry From DM-DMZ Website</h1>
+		const bookingData = `
+		  	<h1>New Customer Booking</h1>
 		  	<h2>Customer Details:</h2>
 		  	<ul>
-			    <li>Name: <b>${req.body.name}</b></li>
-			    <li>Email: <b>${req.body.email}</b></li>
-			    <li>Contact Nr: <b>${req.body.contactNr}</b></li>
-			    <li>Subject: <b>${req.body.subject}</b></li>
+			    <li>First Name     : <b>${req.body.firstName}</b></li>
+					<li>Last Name      : <b>${req.body.lastName}</b></li>
+					<li>Email          : <b>${req.body.email}</b></li>
+					<li>Contact Number : <b>${req.body.contactNr}</b></li>
+				</ul>
+
+				<h2>Booking Details:</h2>
+				<ul>
+			    <li>Check-In       : <b>${req.body.checkInDate}</b></li>
+			    <li>Check-Out      : <b>${req.body.CheckoutDate}</b></li>
+			    <li>Package        : <b>${req.body.package}</b></li>
+					<li>Secondary Package(opt): <b>${req.body.secondaryPackage}</b></li>
 		  	</ul>  
-		  	<h3>Message</h3>
-		  	<p>${req.body.message}</p>
+
+				<h2>Personel Details:</h2>
+				<ul>
+			    <li>Nr of Adults   : <b>${req.body.adults}</b></li>
+			    <li>Nr of Children : <b>${req.body.children}</b></li>
+			    <li>Breakfast      : <b>${req.body.breakfast}</b></li>
+		  	</ul>  
 	  	`;
 		// send user an email
-		const contactDataUser = `
+		const bookingDataUser = `
 				<img src="" width="150"/>
 
-		  	<h1>Thank you for contacting DM-DMZ Guesthouse!</h1>
+		  	<h1>Thank you for booking Accommodation at DM-DMZ Guesthouse!</h1>
 
-		  	<p>Hello ${req.body.name}!,</p>
+		  	<p>Hello ${req.body.firstName} ${req.body.lastName}!,</p>
 
-		  	<p><b>We have received your enquiry. Do not reply to this message!</b></p>
+		  	<p><b>We have received your booking request.</b></p>
 
-		  	<p>A reply will be sent to you as soon as your enquiry has been reviewed.</p>
+				<p><b>To complete your booking, kindly make a Full Payment Deposit to the following bank account:</b></p>
 
-				<p>Happy Vacations,<br>DM-DMZ Guesthouse Contact Team</p>
+		  	<p>Bank   :  <br>Account Number:   <br>Reference: Email Address   </p>
+
+		  	<h2>Customer Details Confirmation:</h2>
+		  	<ul>
+			    <li>First Name     : <b>${req.body.firstName}</b></li>
+					<li>Last Name      : <b>${req.body.lastName}</b></li>
+					<li>Email          : <b>${req.body.email}</b></li>
+					<li>Contact Number : <b>${req.body.contactNr}</b></li>
+				</ul>
+
+				<h2>Booking Details:</h2>
+				<ul>
+			    <li>Check-In       : <b>${req.body.checkInDate}</b></li>
+			    <li>Check-Out      : <b>${req.body.CheckoutDate}</b></li>
+			    <li>Package        : <b>${req.body.package}</b></li>
+					<li>Secondary Package(opt): <b>${req.body.secondaryPackage}</b></li>
+		  	</ul>  
+
+				<h2>Personel Details:</h2>
+				<ul>
+			    <li>Nr of Adults   : <b>${req.body.adults}</b></li>
+			    <li>Nr of Children : <b>${req.body.children}</b></li>
+			    <li>Breakfast      : <b>${req.body.breakfast}</b></li>
+		  	</ul>
+
+				<p><b>Your booking will be confirmed after payment has reflected.</b></p>
+
+				<p>Happy Vacations!</p>
+
+				<p>DM-DMZ Guesthouse</p>
 
 				<p><small><a href="https://www.dm-dmzguesthouse.co.za/privacy-policy">Privacy Policy</a> | <a href="https://www.dm-dmzguesthouse.co.za/terms-of-service">Terms Of Service</a> | <a href="https://www.dm-dmzguesthouse.co.za/contact-us">Contact Us</a></small></p>
 			`;
@@ -338,7 +384,7 @@ module.exports = {
 			port: 465,
 			secure: true,
 			auth: {
-				user: 'booking@dmzguesthouse.co.za',
+				user: 'bookings@dm-dmzguesthouse.co.za',
 				pass: process.env.GMAILPW
 			},
 			tls: {
@@ -352,7 +398,7 @@ module.exports = {
 			port: 465,
 			secure: true,
 			auth: {
-				user: 'booking@dmzguesthouse.co.za',
+				user: 'bookings@dm-dmzguesthouse.co.za',
 				pass: process.env.GMAILPW
 			},
 			tls: {
@@ -362,28 +408,26 @@ module.exports = {
 
 		// email options dmz guesthouse
 		const mailOptions = {
-			from: '"Contact Us - DM-DMZ Guesthouse" <contact@dmzguesthouse.co.za>',
-			to: 'booking@dmzguesthouse.co.za',
+			from: '"Bookings - DM-DMZ Guesthouse" <bookings@dm-dmzguesthouse.co.za>',
+			to: 'bookings@dm-dmzguesthouse.co.za',
 			subject: 'New Customer Enquiry',
-			html: contactData
+			html: bookingData
 		};
 
 		// email options user
 		const mailOptionsUser = {
-			from:
-				'"DM-DMZ Guesthouse - Bookings" <booking@dmzguesthouse.co.za>',
+			from:'"DM-DMZ Guesthouse - Bookings" <bookings@dm-dmzguesthouse.co.za>',
 			to: req.body.email,
-			subject: 'Booking Recieved - DM-DMZ Gusethouse',
-			html: contactDataUser
+			subject: 'Booking Recieved - DM-DMZ Guesthouse',
+			html: bookingDataUser
 		};
 
 		await smtpTransport.sendMail(mailOptions, mailOptionsUser);
 
 		await smtpTransportUser.sendMail(mailOptionsUser);
 
-		req.session.success = `G'day ${req.body
-			.name}, your Booking has been successfully received! An Email has been sent to you.`;
-		res.redirect('/booking');
+		req.session.success = `G'day ${req.body.firstName} ${req.body.lastName}, your Booking request has been received! Please check your Email for finalization.`;
+		res.redirect('/');
 	},
 
 	// GET /terms-of-service
